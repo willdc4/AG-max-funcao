1fa909e2ea14c44d4a0dcd0e6ecb45f802788638
Heuller <heullergit@gmail.com>
2019-10-21 21:50:19 -0300

    criacao dos grupos

--- main.py
+++ main.py
@@ -10,13 +10,30 @@ import random
 
 populacao = []
 
+def torneio(populacao):
+    grupos = []
+    grupo = []
+    i = 1
+    ##divisão da população em 5 grupos contendo 6 integrantes
+    while(i < len(populacao) + 1):
+        grupo.append(populacao[i - 1])
+        if(i % 6 == 0):
+            grupos.append(grupo)
+            grupo = []
+        i += 1
+
+    for grupo in grupos:
+        
+    print(grupos)
+
+
 ##para representar de 0 a 10 é preciso 4 bits 
 for i in range(30):
     x = random.randint(-10,10) #valor de x varia de -10 a 10
-    print('ANTES', x)
     x = f'{x:05b}' # formatar o inteiro para binário com 5 dígitos
-    print('DEPOIS', x)
     x = x.replace('-','1')
     populacao.append(x)
 
-print(populacao)
+
+
+torneio(populacao)
