20d3f831782466bd6e61325d21c019a1f8c69a4e
willdc4 <williamdavicoelho@gmail.com>
2019-10-22 15:29:03 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-'''random x
-populacao inicial 30
-mutacao = 0.01
-crossover = 0.7
-selecao por torneio
-num_geracoes = 5
-f(x) = (x * x) - (3 * x) + 4
-'''
 import random
 
 # Converte o valor 'x' de binário para decimal
@@ -20,38 +12,100 @@ def binario_para_decimal(x):
 
 #Função objetivo do problema
 def funcao_objetivo(x):
-    x = int(x)
+    x = binario_para_decimal(x)
     return (x * x) - (3 * x) + 4
 
 def torneio(populacao, num_individuos):
-    #escolher para o torneio
     vencedor = None
     
     for i in range(num_individuos):
-        individuo = random.choice(populacao)
+        individuo = random.choice(populacao) # Escolhe um indivíduo aleatoriamente para o torneio
+        # Escolhe o indivíduo com a maior função objetivo dentre os escolhidos
         if (vencedor is None) or funcao_objetivo(individuo) > funcao_objetivo(vencedor):
             vencedor = individuo
+        #print(individuo, funcao_objetivo(individuo))
     return vencedor
 
-'''
+# Calcula a probabilidade e efetua a mutação caso necessário
+def mutacao(individuo):
+    novo_individuo = ""
+    for digito in individuo:
+        prob_mutacao = random.uniform(0.0,1.0)
+        if (prob_mutacao < 0.01): # Probabilidade de mutação
+            # Invertendo os bits
+            if (digito == '1'):
+                novo_individuo+='0'
+            else:
+                novo_individuo+='1'
+        else:
+            novo_individuo += digito
+    return novo_individuo
+
 def crossover(pais):
-    ponto_corte = random.randint(1,4)
-    pai1 = random.choice(populacao)
-'''
+    num_pares = len(pais)//2
+    nova_populacao = []
+    for i in range(num_pares):
+        ponto_corte = random.randint(1,4)
+        prob_crossover = random.uniform(0.0,1.0)
+        # Escolhe os pais
+        pai_1 = random.choice(pais)
+        pais.remove(pai_1)
+        pai_2 = random.choice(pais)
+        pais.remove(pai_2)
+        #print("tentativa", i)
+        #print("pais",pai_1,pai_2)
+        if (prob_crossover < 0.7): # Probabilidade de ocorrer o crossover
+            # Combina os cromossomos dos pais
+            filho_1 = pai_1[0:ponto_corte] + pai_2[ponto_corte:]
+            filho_2 = pai_2[0:ponto_corte] + pai_1[ponto_corte:]
+            # Mutação
+            filho_1 = mutacao(filho_1)
+            filho_2 = mutacao(filho_2)            
+            # Verifica se o valor dos filhos se encontra no intervalo pré-definido: [-10,10]
+            valor = binario_para_decimal(filho_1)
+            if (valor > 10) or (valor < -10):
+                filho_1 = pai_1
+            valor = binario_para_decimal(filho_2)
+            if (valor > 10) or (valor < -10):
+                filho_2 = pai_2
+            
+            #print('===crossover===')
+            #print('ponto de corte: ',ponto_corte)
+            nova_populacao.append(filho_1)
+            nova_populacao.append(filho_2)
+            #print("filhos",filho_1,filho_2,"\n")
+            
+        else:
+            nova_populacao.append(pai_1)
+            nova_populacao.append(pai_2)
+    return nova_populacao 
+
 populacao = []
 
+# Gerando a população inicial
 for i in range(30):
     x = random.randint(-10,10)
     x = f'{x:05b}' # formatar o inteiro para binário com 5 dígitos
     x = x.replace('-','1')
     populacao.append(x)
+print("População inicial:\n", populacao, "\n")
 
-#print(populacao)
 # Condição de parada 20 gerações
-for i in range(1):
+for i in range(20):
     pais = []
-    for j in range(20): # Quantidade de pais a serem selecionados
+    for j in range(30): # Quantidade de pais a serem selecionados
         pais.append(torneio(populacao,2))
-        
+    populacao.clear()
+    populacao = crossover(pais)
+    print( "Geração", i + 1, ":\n\n",populacao,"\n")
 
-    print(pais)
+funcao_max = 0
+x_funcao_max = 0
+# Calculando a melhor solução da população resultando, ou seja, o melhor indíviduo
+for x in populacao:
+    if(funcao_objetivo(x) > funcao_max):
+        funcao_max = funcao_objetivo(x)
+        x_funcao_max = binario_para_decimal(x)
+
+print("\nMelhor x =",x_funcao_max)
+print("Função máxima =",funcao_max)
